cfbe6c7afbe21e3ace6e26e77e96bcc2a923cfeb
NicoRondan <nicolas@toptive.co>
2021-12-15 02:34:20 -0300

    added launch screen for android successfully

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen';
 import HomeScreen from './src/screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">

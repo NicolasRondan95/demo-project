f0494ade24d6d1675c236a413ced38a2cd5d97ab
NicoRondan <nicolas@toptive.co>
2021-12-15 03:44:05 -0300

    adding the company logo on home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Image} from 'react-native';
 import Button from '../components/Button';
+import Logo from '../assets/toptive_logo_purple_white_256px.png';
 
 const HomeScreen = ({navigation}) => {
   const {navigate} = navigation;
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={Logo} />
+      </View>
       <Button title="Go to Camera Demo" onPress={() => navigate('Camera')} />
       <Button
         title="Go to Location Demo"
@@ -20,7 +24,17 @@ const HomeScreen = ({navigation}) => {
 };
 
 const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'center', padding: 16},
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  imageContainer: {justifyContent: 'center', alignItems: 'center', bottom: 25},
+  image: {
+    height: 150,
+    justifyContent: 'center',
+    resizeMode: 'contain',
+  },
 });
 
 export default HomeScreen;

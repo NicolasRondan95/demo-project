bfca14f79caaf2e8235e7c6f121ae178d337fea7
lucasarmas <lucasarmas@toptive.co>
2022-01-16 18:54:38 -0300

    Add LocationDemoScreen

--- src/screens/LocationDemoScreen.js
+++ src/screens/LocationDemoScreen.js
@@ -1,83 +1,168 @@
-import React, {useState} from 'react';
-import {Dimensions, StyleSheet} from 'react-native';
-import MapView from 'react-native-maps';
-import MapViewDirections from 'react-native-maps-directions';
+import React, {useState, useEffect, useCallback, useRef} from 'react';
+import {
+  Dimensions,
+  StyleSheet,
+  View,
+  PermissionsAndroid,
+  Button,
+} from 'react-native';
+import RNMapView, {Marker} from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
 
 const {width, height} = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
-const LATITUDE = -33.132876;
-const LONGITUDE = -64.352561;
-const LATITUDE_DELTA = 0.02;
+const LATITUDE_DELTA = 0.009;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-const INITIAL_COORDINATES = [
-  {
-    // TopTive
-    latitude: LATITUDE,
-    longitude: LONGITUDE,
-  },
-  {
-    // Clinica regional del sud
-    latitude: -33.129095953279666,
-    longitude: -64.35136312372927,
-  },
-];
-const GOOGLE_MAPS_APIKEY = 'AIzaSyCj9uvC5MIY9dS7vA3cdHNRyP3W2phMj5o';
 
 const LocationDemoScreen = () => {
-  const [coordinates, setCoordinates] = useState(INITIAL_COORDINATES);
+  const [location, setLocation] = useState({
+    coords: {latitude: 0, longitude: 0},
+  });
+  const [tracking, setTracking] = useState(false);
 
-  const onMapPress = e => {
-    setCoordinates([...coordinates, e.nativeEvent.coordinate]);
+  const watchId = useRef(null);
+
+  useEffect(() => {
+    return () => {
+      removeLocationTracking();
+    };
+  }, [removeLocationTracking]);
+
+  const requestPermissions = async () => {
+    const hasPermission = await PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+
+    if (hasPermission) {
+      return true;
+    }
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+
+      if (
+        granted === PermissionsAndroid.RESULTS.GRANTED ||
+        granted === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  return (
-    <MapView
-      initialRegion={{
-        latitude: LATITUDE,
-        longitude: LONGITUDE,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      }}
-      style={StyleSheet.absoluteFill}
-      ref={c => (this.mapView = c)}
-      onPress={onMapPress}>
-      {coordinates.map((coordinate, index) => (
-        <MapView.Marker key={`coordinate_${index}`} coordinate={coordinate} />
-      ))}
-      {coordinates.length >= 2 && (
-        <MapViewDirections
-          origin={coordinates[0]}
-          waypoints={coordinates.length > 2 ? coordinates.slice(1, -1) : []}
-          destination={coordinates[coordinates.length - 1]}
-          apikey={GOOGLE_MAPS_APIKEY}
-          strokeWidth={3}
-          strokeColor="hotpink"
-          optimizeWaypoints={true}
-          onStart={params => {
-            console.log(
-              `Started routing between "${params.origin}" and "${params.destination}"`,
-            );
-          }}
-          onReady={result => {
-            console.log(`Distance: ${result.distance} km`);
-            console.log(`Duration: ${result.duration} min.`);
+  const getLocation = async () => {
+    const hasPermission = await requestPermissions();
 
-            this.mapView.fitToCoordinates(result.coordinates, {
-              edgePadding: {
-                right: width / 20,
-                bottom: height / 20,
-                left: width / 20,
-                top: height / 20,
-              },
-            });
-          }}
-          onError={errorMessage => {
-            console.log('GOT AN ERROR');
-          }}
+    if (!hasPermission) {
+      return;
+    }
+
+    Geolocation.getCurrentPosition(
+      position => {
+        setLocation(position);
+        console.log('GET LOCATION: ', position.coords);
+      },
+      error => {
+        setLocation(null);
+        console.log(error);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 15000,
+        maximumAge: 10000,
+        distanceFilter: 0,
+      },
+    );
+  };
+
+  const trackLocation = async () => {
+    const hasPermission = await requestPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    setTracking(true);
+
+    watchId.current = Geolocation.watchPosition(
+      position => {
+        console.log('TRACK LOCATION: ', position.coords);
+        setLocation(position);
+      },
+      error => {
+        console.log(error);
+        setLocation(null);
+      },
+      {
+        enableHighAccuracy: true,
+        distanceFilter: 0,
+        interval: 5000,
+        fastestInterval: 2000,
+      },
+    );
+    console.log('WATCH ID: ', watchId.current);
+  };
+
+  const removeLocationTracking = useCallback(() => {
+    if (watchId.current !== null) {
+      Geolocation.clearWatch(watchId.current);
+      watchId.current = null;
+      setTracking(false);
+    }
+  }, []);
+
+  return (
+    <View style={styles.mainWrapper}>
+      <View style={styles.mapWrapper}>
+        <RNMapView
+          style={StyleSheet.absoluteFillObject}
+          region={{
+            latitude: location?.coords.latitude,
+            longitude: location?.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}>
+          <Marker coordinate={location.coords} />
+        </RNMapView>
+      </View>
+      <View style={styles.controlsWrapperr}>
+        <Button title="Get Location" color={'#5041E8'} onPress={getLocation} />
+        <Button
+          title="Start Observing"
+          color={'#5041E8'}
+          onPress={trackLocation}
+          disabled={tracking}
         />
-      )}
-    </MapView>
+        <Button
+          title="Stop Observing"
+          color={'#5041E8'}
+          onPress={removeLocationTracking}
+          disabled={!tracking}
+        />
+      </View>
+    </View>
   );
 };
 
 export default LocationDemoScreen;
+
+const styles = StyleSheet.create({
+  mainWrapper: {
+    flex: 1,
+    backgroundColor: '#D0D0D0',
+  },
+  mapWrapper: {
+    flex: 1,
+  },
+  controlsWrapperr: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginVertical: 12,
+    width: '100%',
+  },
+});
